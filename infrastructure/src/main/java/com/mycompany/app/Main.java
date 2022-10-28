356e94d8437085ccb60c837ea59bf3fb65f041f2
seongchan.hwang <seongchan.hwang@navercorp.com>
2022-10-28 23:23:44 +0900

    add cdktf project

---
+++ infrastructure/src/main/java/com/mycompany/app/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+import com.hashicorp.cdktf.App;
+
+public class Main
+{
+    public static void main(String[] args) {
+        final App app = new App();
+        new MainStack(app, Constant.GITHUB_REPOSITORY);
+        app.synth();
+    }
+}
