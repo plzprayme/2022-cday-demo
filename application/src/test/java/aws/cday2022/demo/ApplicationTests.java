bf93f906c1f9c60f1952ad1128915da06f9d8f09
seongchan.hwang <seongchan.hwang@navercorp.com>
2022-10-28 23:17:01 +0900

    add application

---
+++ application/src/test/java/aws/cday2022/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.cday2022.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
